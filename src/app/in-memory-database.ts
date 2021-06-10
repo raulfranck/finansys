21d308518f05fe8dc6d02186099cbf5536a1521d
Raul Franck Silva <raul.silva@dotzmkt.com.br>
2021-06-09 21:13:57 -0300

    Criando model de categories

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model'
+
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             {id: 1, name: "Lazer", description: "Cinema, viagens, bares e restaurantes, etc"},
             {id: 2, name: "Saúde", description: "Convênio, Remédios e gastos similares"},
             {id: 3, name: "Moradia", description: "Aluguel, contas do lar(internet, água, luz), financiamento do imóvel"},
